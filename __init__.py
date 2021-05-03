94b8348d38b67560b339c769019d1290278d893a
nan-yi <nan-yi@qq.com>
2021-05-03 18:05:14 +0800

    数字货币量化交易策略

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/30 11:24
+# @Author  : Ryan
+# @Site    :
+# @File    : __init__.py
+# @Software: PyCharm
---
+++ app/authorization.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/30 11:24
+# @Author  : Ryan
+# @Site    :
+# @File    : authorization.py
+# @Software: PyCharm
+
+# Get an Key and Secret
+# https://www.binance.com/restapipub.html
+
+# 测试账号
+api_key = ''
+api_secret = ''
+
+
+recv_window = 5000
+
+dingding_token = "" # 钉钉群token, 接收服务器状态信息
+dingding_token2 = "" # 钉钉群token, 接收交易信息
