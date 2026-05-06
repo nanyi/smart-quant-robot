764ded8680dc760096dbf24b03e39d5d896483d3
ryan <nan-yi@qq.com>
2026-05-06 20:50:25 +0800

    feat(strategy): 添加策略基类SignalStrategy

---
+++ strategy/base.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Optional
+from enum import IntEnum
+
+
+class SignalType(IntEnum):
+    """信号类型"""
+    NEUTRAL = 0
+    BUY = 1
+    SELL = -1
+
+
+@dataclass
+class Signal:
+    """交易信号"""
+    signal_type: SignalType
+    strategy_name: str
+    weight: float
+    price: float
+    time: str
+    confidence: float = 1.0
+
+
+class SignalStrategy(ABC):
+    """策略基类"""
+    
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """策略名称"""
+        pass
+    
+    @property
+    def weight(self) -> float:
+        """策略权重"""
+        return 1.0
+    
+    @abstractmethod
+    def calculate(self, df):
+        """计算交易信号
+        
+        :param df: K线数据 DataFrame
+        :return: Signal 或 None
+        """
+        pass
